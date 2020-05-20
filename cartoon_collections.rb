7e38bb7539e34d6dc69a8f457420c78df4944290
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-20 22:34:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@
 
 def roll_call_dwarves(dwarfs)# code an argument here
   # Your code here
-  #dwarfs = ["Doc", "Dopey", "Bashful", "Grumpy"]
+  dwarfs = ["Doc", "Dopey", "Bashful", "Grumpy"]
   dwarfs.each_with_index do | name , index |
     puts "#{index + 1} #{name}"
   end
